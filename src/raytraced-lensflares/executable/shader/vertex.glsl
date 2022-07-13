44ab8d868ab9c0e1240523d095363350b42b05c4
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-13 14:25:04 +0200

    feat: basic lens raytracing

--- src/raytraced-lensflares/executable/shader/vertex.glsl
+++ src/raytraced-lensflares/executable/shader/vertex.glsl
@@ -15,13 +15,67 @@ struct Lens{
     float ior;
     float apertureRadius;
     float center;
-} lens;
+};
 
 layout(std430, binding = 4) buffer lensSystem
 {
     Lens lenses[];
 };
 
+struct Intersection{
+    vec3 position;
+    vec3 normal;
+};
+
+Intersection intersectLens(inout Lens lens, vec3 position, vec3 direction)  {
+    vec3 D = position - vec3(0, 0, lens.center - lens.curvatureRadius);
+    float B = dot(D, direction);
+    float C = dot(D, D) - lens.curvatureRadius * lens.curvatureRadius;
+    float B2_C = B * B - C;
+    if (B2_C < 0) {
+        return Intersection(vec3(-1), vec3(-1));
+    }
+    float sgn = (lens.curvatureRadius * direction.z) > 0 ? 1 : -1;
+    float t = sqrt(B2_C) * sgn - B;
+    vec3 intersectionPosition = direction * t + position;
+    return Intersection(intersectionPosition, normalize(intersectionPosition - vec3(0, 0, lens.center - lens.curvatureRadius)));
+}
+
+vec3 refract(inout Intersection intersection, vec3 direction, float iorIn, float iorOut){
+    float cosi = clamp(dot(direction, intersection.normal), -1.0f, 1.0f);
+    vec3 n = intersection.normal;
+    if (cosi < 0) {
+        cosi = -cosi;
+    } else {
+        float temp = iorIn;
+        iorIn = iorOut;
+        iorOut = temp;
+        n = -intersection.normal;
+    }
+    float eta = iorIn / iorOut;
+    float k = 1 - eta * eta * (1 - cosi * cosi);
+    return k < 0 ? vec3(0, 0, 0) : eta * direction + (eta * cosi - sqrt(k)) * n;
+}
+
 void main(){
-    gl_Position = vPos;
+    const float startDistance = 200;
+
+    vec3 tracedPosition = vPos * 15 + vec3(0.0, 0.0, 200.0);
+    vec3 tracedDirection = vec3(0, 0, -1);
+    float ior =1;
+    for (int i=0;i< lenses.length();i++){
+        Lens lens = lenses[i];
+        if (lens.ior == 0) {
+            continue;
+        }
+        Intersection intersection = intersectLens(lens, tracedPosition, tracedDirection);
+        tracedPosition = intersection.position;
+
+        tracedDirection = refract(intersection, tracedDirection, ior, lens.ior);
+        ior = lens.ior;
+    }
+    float t = (-tracedPosition.z) / tracedDirection.z;
+    vec3 positionOnPlane = tracedPosition + tracedDirection * t;
+
+    gl_Position = vec4(positionOnPlane, 1);
 }
