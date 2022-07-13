f0110c71ed7eb8988e2b6ea503b84b4f4eb17149
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-13 11:19:40 +0200

    refactor: cleanup shaders

--- src/raytraced-lensflares/executable/shader/vertex.glsl
+++ src/raytraced-lensflares/executable/shader/vertex.glsl
@@ -3,7 +3,6 @@
 uniform mat4 MVP;
 in vec3 vCol;
 in vec3 vPos;
-out vec3 color;
 
 layout(std430, binding = 3) buffer reflectionSequences
 {
@@ -24,6 +23,5 @@ layout(std430, binding = 4) buffer lensSystem
 };
 
 void main(){
-    gl_Position = MVP * vec4(vPos, 1.0);
-    color = vCol;
+    gl_Position = vPos;
 }
