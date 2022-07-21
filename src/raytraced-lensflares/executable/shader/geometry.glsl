93339936ea1189452e7fc40184f1b6a120436555
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-21 11:34:38 +0200

    feat: calculate intensity for fixed grid

--- src/raytraced-lensflares/executable/shader/geometry.glsl
+++ src/raytraced-lensflares/executable/shader/geometry.glsl
@@ -7,12 +7,19 @@ layout(max_vertices=3) out;
 in vec3 vcColor[];
 
 out vec3 gColor;
+out float intensity;
 
 void main()
 {
+
+    vec3 v0v1 = vec3(gl_in[1].gl_Position - gl_in[0].gl_Position);
+    vec3 v0v2 = vec3(gl_in[2].gl_Position - gl_in[0].gl_Position);
+    float calculatedArea = length(cross(v0v1, v0v2))/2;
+    float originalArea =0.1*0.1/2;// todo when changing grid resolution, adjust this!
     for (int i = 0; i < 3; i++) {
         gl_Position = gl_in[i].gl_Position;
-        gColor = vcColor[i];
+        intensity = originalArea/calculatedArea;
+        gColor = vcColor[i] * intensity;
         //color = vec3(1,0,0);
         EmitVertex();
     }
