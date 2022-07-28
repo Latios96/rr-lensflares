08509bee7f565b13dce728b6804825b783ac9522
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-28 12:14:06 +0200

    fix: dont calculate new for every vertex

--- src/raytraced-lensflares/executable/shader/geometry.glsl
+++ src/raytraced-lensflares/executable/shader/geometry.glsl
@@ -18,9 +18,9 @@ void main() {
   vec3 v0v2 = vec3(gl_in[2].gl_Position - gl_in[0].gl_Position);
   float calculatedArea = length(cross(v0v1, v0v2)) / 2;
   float originalArea = 0.1 * 0.1;// todo when changing grid resolution, adjust this!
+  intensity = originalArea / calculatedArea;
   for (int i = 0; i < 3; i++) {
     gl_Position = gl_in[i].gl_Position;
-    intensity = originalArea / calculatedArea;
     intensity = rayStayed[i] * intensity;
     textureCoordinate = vAperturePos[i];
     EmitVertex();
