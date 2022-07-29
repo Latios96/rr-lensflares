fa52ff2d91cd6dc46a2ad9525a1328e8d251acdf
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-07-29 12:34:10 +0200

    refactor: use reserve

--- src/raytraced-lensflares/lib/GridGenerator.cpp
+++ src/raytraced-lensflares/lib/GridGenerator.cpp
@@ -1,4 +1,5 @@
 #include "GridGenerator.h"
+#include <spdlog/spdlog.h>
 
 void GridGenerator::generateGrid(Mesh &mesh, int subdivs) {
   generatePoints(mesh, subdivs);
@@ -7,16 +8,20 @@ void GridGenerator::generateGrid(Mesh &mesh, int subdivs) {
 
 void GridGenerator::generatePoints(Mesh &mesh, int subdivs) {
   const float subdivStep = 1.0f / subdivs;
-
+  mesh.vertices.reserve((subdivs + 1) * (subdivs + 1));
+  spdlog::info("reserved {} for points", mesh.vertices.capacity());
   for (int y = 0; y <= subdivs; y++) {
     for (int x = 0; x <= subdivs; x++) {
       mesh.vertices.push_back(
           Vertex(glm::vec3(-0.5f + subdivStep * x, -0.5f + subdivStep * y, 0.0f)));
     }
   }
+  spdlog::info("used {} for points", mesh.vertices.size());
 }
 
 void GridGenerator::generateIndices(Mesh &mesh, int subdivs) {
+  mesh.indices.reserve(subdivs * subdivs * 3 * 2);
+  spdlog::info("reserved {} for indices", mesh.indices.capacity());
   for (int row = 0; row < subdivs; row++) {
     int startIndexForRow = 0 + row * (subdivs + 1);
     for (int quad = 0; quad < subdivs; quad++) {
@@ -30,4 +35,5 @@ void GridGenerator::generateIndices(Mesh &mesh, int subdivs) {
       mesh.indices.push_back(startIndexForQuad + subdivs + 1);
     }
   }
+  spdlog::info("used {} for indices", mesh.indices.size());
 }
