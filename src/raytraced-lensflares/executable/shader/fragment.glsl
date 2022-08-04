e48df02ba7d89eca871a4525acce10e1fd95b41a
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-08-04 13:51:00 +0200

    remove dead code

--- src/raytraced-lensflares/executable/shader/fragment.glsl
+++ src/raytraced-lensflares/executable/shader/fragment.glsl
@@ -7,7 +7,5 @@ out vec4 fragment;
 uniform sampler2D apertureTexture;
 
 void main() {
-    //fragment = vec4(textureCoordinate.x, textureCoordinate.y, 0, 1.0) * intensity;
     fragment = texture(apertureTexture, textureCoordinate) * intensity;
-    //fragment = vec4(intensity, intensity, 0, 1.0);
 }
